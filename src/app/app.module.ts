f407f5b8f6aa9d41c4a05c0d01d5a7f169a6e493
Alpaslan Dulkadir <alpaslandulkadir@gmail.com>
2021-06-10 15:24:40 +0300

    AdminModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { MainModule } from './pages/main.module';
-
+import {AdminModule} from './admin-pages/admin.module';
 import { AppComponent } from './app.component';
 
-import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
-import { AdminNavComponent } from './nav/admin-nav/admin-nav.component';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MainModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MainModule,AdminModule],
   providers: [],
   bootstrap: [AppComponent],
 })
